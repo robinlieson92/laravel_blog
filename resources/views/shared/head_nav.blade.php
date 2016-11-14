d21e4f17751c1ba36763aebbd452fe51acd0ffc9
=Robin Lieson <=robinlieson92@gmail.com>
2016-11-14 10:45:55 +0700

    Modified Navbar

--- resources/views/shared/head_nav.blade.php
+++ resources/views/shared/head_nav.blade.php
@@ -1,21 +1,20 @@
 <div class="navbar navbar-fixed-top navbar-default" role="navigation">
   <div class="container">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"/>
-        <span class="icon-bar"/>
-        <span class="icon-bar"/>
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
       </button>
-      <a href="#" class = "navbar-brand">Robin Lieson</a>
+      {!! link_to(route('root'),"Robin Lieson", ["class"=>"navbar-brand"]) !!}
     </div>
-    <div class="collapse navbar-collapse">
-    <ul class="nav navbar-nav navbar-right">
-      <li>{!! link_to(route('root'), "Home") !!}</li>
-      <li><a>Profile</a></li>
-      <li>{!! link_to(route('articles.index'), "Article") !!}</li>
-      
-    </ul>
+    <div class="navbar-collapse navbar-responsive-collapse">
+      <ul class="nav navbar-nav navbar-right">
+        <li>{!! link_to(route('root'), "Home") !!}</li>
+        <li><a>Profile</a></li>
+        <li>{!! link_to(route('articles.index'), "Article") !!}</li>
+        
+      </ul>
     </div>
   </div>
 </div>
